d141529b9e4614076640483381f6e9552f2be1ae
Christopher Chang <chiggiechang@gmail.com>
2015-03-23 22:15:34 -0700

    Adding on a bit more.

---
+++ run.bat
@@ -0,0 +1,3 @@
+java -Xms3072M -Xmx3072M -XX:MaxPermSize=128M -jar HomeNahdras.jar
+PAUSE
+REM This is not working if you run from your Windows Explorer, so I would just copy/paste this to a new .bat file with the Jar built.
